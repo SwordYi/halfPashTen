a572b17d6a8be39ccdc068ab60081d3b35b7ec68
SwordYi <283901189@qq.com>
2018-12-18 13:47:38 +0800

    first create

---
+++ halfPastTen.py
@@ -0,0 +1,8 @@
+"""
+    ProductName:Half Past Ten
+    Author:Sword
+    ModefiedTime:2018-12-18
+"""
+print("----------------------点半游戏----------------------")
+playName = input("请输入玩家姓名：")
+print(playName + "，您好！欢迎光临十点半游戏")
